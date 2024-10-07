8ea82184c2b036f675d632da02b3e5dfd67c4ae4
MrFrangipane <contact@frangipane.me>
2024-10-07 13:14:11 +0200

    feat(ui): basic main window

--- midimiddleware/components/launcher.py
+++ midimiddleware/components/launcher.py
@@ -1,15 +1,25 @@
-import os.path
+import sys
 
+from PySide6.QtWidgets import QApplication
+from pyside6helpers import css
+from pyside6helpers import resources as ps6_resources
+
+from midimiddleware.components import resources
+from midimiddleware.components.ui.main_window import MainWindow
 from midimiddleware.core.components import Components
 
 
 class Launcher:
 
     def __init__(self):
-        Components().configuration.resources_folder = os.path.join(
-            os.path.dirname(os.path.dirname(__file__)),
-            "resources"
-        )
+        Components().configuration.resources_folder = resources.make_path()
+        ps6_resources.set_root(resources.make_path())  # PyInstaller does not bundle modules resources ?
 
     def exec(self):
-        pass
+        app = QApplication()
+        css.load_onto(app)
+
+        main_window = MainWindow()
+        main_window.show()
+
+        sys.exit(app.exec())
